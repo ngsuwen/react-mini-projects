cf75e825276573e838f7767761bca9e8918a5343
LAPTOP-KH9KVP83\Suwen <ng.suwen@dhs.sg>
2021-10-22 17:29:58 +0800

    build

--- counter/README.md
+++ counter/README.md
@@ -1,4 +1,4 @@
-# GA Lab: Counter
+# GA Lab: Colored Divs
 https://ngsuwen.github.io/react-mini-projects/counter/build/
 ## Technologies
-* **ReactJS** - useReducer
+* **ReactJS** - useEffect
---
+++ counter/build/precache-manifest.6bd0daa1607168e991454f332d4d0fd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fbb0513e3b27c55dcdd78cec7e61733",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c87edd2e6b5a28ceecfc",
+    "url": "./static/css/main.89444ba1.chunk.css"
+  },
+  {
+    "revision": "3ba0dd3a084769a325eb",
+    "url": "./static/js/2.076e3a33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.076e3a33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c87edd2e6b5a28ceecfc",
+    "url": "./static/js/main.2db639ee.chunk.js"
+  },
+  {
+    "revision": "4501e2c489d8af8aa658",
+    "url": "./static/js/runtime-main.ec9d1e7c.js"
+  }
+]);
