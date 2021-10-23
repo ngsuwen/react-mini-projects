1b33397c482910e8d394cbc320aab06752e1f77b
LAPTOP-KH9KVP83\Suwen <ng.suwen@dhs.sg>
2021-10-24 03:24:27 +0800

    build

---
+++ istock/build/precache-manifest.99e9b100c74f22a2ed3743553b3be0c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cde781688af5d4acb58aa2dd64c5968",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6f1376143ccff514fe70",
+    "url": "./static/css/main.58a2a755.chunk.css"
+  },
+  {
+    "revision": "4e808529a9bf185e71e7",
+    "url": "./static/js/2.c7c8947d.chunk.js"
+  },
+  {
+    "revision": "6f1376143ccff514fe70",
+    "url": "./static/js/main.6d23402c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
