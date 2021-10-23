42e23186038b08862bbc2e464e5bc11ad1f5f6ab
LAPTOP-KH9KVP83\Suwen <ng.suwen@dhs.sg>
2021-10-23 11:08:45 +0800

    build

---
+++ dentist/build/precache-manifest.e0ffefafe98115bb615616e15fe19954.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e44cde5f1b4fb75312a5dce9db681c8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "92d6450db0d46e809cb8",
+    "url": "./static/css/main.3be36971.chunk.css"
+  },
+  {
+    "revision": "8f5a335ebfb792093162",
+    "url": "./static/js/2.20236a68.chunk.js"
+  },
+  {
+    "revision": "92d6450db0d46e809cb8",
+    "url": "./static/js/main.9a84ab34.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
